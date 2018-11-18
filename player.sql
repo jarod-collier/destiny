8c6ec9769dd07e82babc2e9345a50469faf934b1
Rand3rino <37269917+Rand3rino@users.noreply.github.com>
2018-11-18 17:30:59 -0500

    Create player.sql

---
+++ player.sql
@@ -0,0 +1,18 @@
+-- Add the players
+--
+insert into player values ('Farmer', '5', '15',	'10',	'1', NULL,	NULL);
+insert into player values ('Butcher',	'5',	'20',	'5',	'1',	NULL,	NULL);
+insert into player values ('Pawn',	'15',	'10',	'15',	'3',	'The Will of Crotia',	NULL);
+insert into player values ('Servant',	'45',	'12',	'13',	'9',	'The Will of Crotia',	NULL);
+insert into player values ('Minion',	'95',	'30',	'20',	'19',	'The Will of Crotia',	'Vault of Glass');
+insert into player values ('Assassin',	'110',	'20',	'55',	'22',	NULL,	'Vault of Glass');
+insert into player values ('Knight',	'125',	'5',	'70',	'25',	NULL,	'Vault of Glass);
+insert into player values ('Bishop',	'135',	'25',	'50',	'27',	'Dust Palace',	NULL);
+insert into player values ('Lancer',	'150',	'55',	'20', '30',	NULL,	'Vault of Glass');
+insert into player values ('Archer',	'165',	'50',	'50', '33',	'Dust Palace',	NULL);
+insert into player values ('Rook',	'180',	'50',	'50',	'36',	NULL,	'Crota's End');
+insert into player values ('Ruler',	'200',	'30',	'70',	'40',	'Dust Palace',	'Crota's End');
+insert into player values ('Queen',	'200',	'50',	'50',	'40',	NULL,	'Crota's End');
+insert into player values ('King',	'200',	'90',	'10',	'40',	NULL,	'Crota's End');
+--
+--
