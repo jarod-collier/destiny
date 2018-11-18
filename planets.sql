1c1ee3ae5d12e8e9e77ab08c92ed48d99af4b13c
Josef Hartsough <36377458+JosefHartsough@users.noreply.github.com>
2018-11-18 15:33:35 -0500

    Create planets.sql

---
+++ planets.sql
@@ -0,0 +1,17 @@
+/* This file will populate the Planets table*/
+
+/* Data for Earth */
+INSERT INTO planet (name, material, enemType, enviro)
+VALUES (Earth, Spin Metal, Hive);
+
+/* Data for Moon */
+INSERT INTO planet (name, material, enemType, enviro)
+VALUES (Moon, Helium Fragments, Fallen);
+
+/* Data for Venus */
+INSERT INTO planet (name, material, enemType, enviro)
+VALUES (Venus, Spirit Bloom , Vex);
+
+/* Data for Mars */
+INSERT INTO planet (name, material, enemType, enviro)
+VALUES (Mars, Relic Iron, Cabal);
