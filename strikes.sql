99ffbecb15e3cbd973af8421c218cbab139361f9
joshweinmann <35538626+joshweinmann@users.noreply.github.com>
2018-11-18 15:49:31 -0500

    Added strike insert statements

---
+++ strikes.sql
@@ -0,0 +1,25 @@
+--Inserts data for strikes
+
+INSERT INTO strike (name, boss, dfclty, pname, charAmt)
+VALUES ("The Abomination Heist", "Hive Abomination", "Normal", "Moon", 3);
+
+INSERT INTO strike (name, boss, dfclty, pname, charAmt)
+VALUES ("Fallen Saber", "SABER 2", "Easy", "Earth", 3);
+
+INSERT INTO strike (name, boss, dfclty, pname, charAmt)
+VALUES ("Dust Palace", "Vatch", "Normal", "Mars", 3);
+
+INSERT INTO strike (name, boss, dfclty, pname, charAmt)
+VALUES ("Echo Chamber", "Theosyion", "Hard", "Venus", 3);
+
+INSERT INTO strike (name, boss, dfclty, pname, charAmt)
+VALUES ("Winter's Run", "Aksor", "Hard", "Venus", 3);
+
+INSERT INTO strike (name, boss, dfclty, pname, charAmt)
+VALUES ("The Undying Mind", "The Undying Mind", "Normal", "Mars", 3);
+
+INSERT INTO strike (name, boss, dfclty, pname, charAmt)
+VALUES ("The Shadow Thief", "Taniks Perfected", "Normal", "Moon", 3);
+
+INSERT INTO strike (name, boss, dfclty, pname, charAmt)
+VALUES ("The Will of Crotia", "Omnigul", "Easy", "Earth", 3);
