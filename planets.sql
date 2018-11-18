86c1edc8e4f3f5bfa93784a065bf66d3a09461b9
Josef Hartsough <36377458+JosefHartsough@users.noreply.github.com>
2018-11-18 15:36:33 -0500

    Update planets.sql

--- planets.sql
+++ planets.sql
@@ -1,17 +1,17 @@
 /* This file will populate the Planets table*/
 
 /* Data for Earth */
-INSERT INTO planet (name, material, enemType, enviro)
+INSERT INTO planet (name, material, enemType)
 VALUES (Earth, Spin Metal, Hive);
 
 /* Data for Moon */
-INSERT INTO planet (name, material, enemType, enviro)
+INSERT INTO planet (name, material, enemType)
 VALUES (Moon, Helium Fragments, Fallen);
 
 /* Data for Venus */
-INSERT INTO planet (name, material, enemType, enviro)
+INSERT INTO planet (name, material, enemType)
 VALUES (Venus, Spirit Bloom , Vex);
 
 /* Data for Mars */
-INSERT INTO planet (name, material, enemType, enviro)
+INSERT INTO planet (name, material, enemType)
 VALUES (Mars, Relic Iron, Cabal);
